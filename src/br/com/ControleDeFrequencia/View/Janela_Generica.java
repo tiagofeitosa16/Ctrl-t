0b3de6520f0e30be47311be59f7d7f612b62080e
Jerffeson Goncalves <jerffeson@localhost.localdomain>
2017-06-05 21:09:11 -0300

    Bugfix janela_generica como modo composer

--- src/br/com/ControleDeFrequencia/View/Janela_Generica.java
+++ src/br/com/ControleDeFrequencia/View/Janela_Generica.java
@@ -35,7 +35,7 @@ public class Janela_Generica extends javax.swing.JFrame {
         jScrollPane2 = new javax.swing.JScrollPane();
         jTable2 = new javax.swing.JTable();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jButtonInserir.setText("Inserir");
         jButtonInserir.addActionListener(new java.awt.event.ActionListener() {
